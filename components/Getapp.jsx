c2f449461747fe618254174d958bfd8e0a7b55ab
[Kevin Osei Kwaku] <[knokwaku@gmail.com]>
2022-08-17 13:56:22 -0700

    changed getapp

--- components/Getapp.jsx
+++ components/Getapp.jsx
@@ -6,7 +6,8 @@ const Getapp = () => {
     return (
         <div className='h-[250px] flex'>
             <div className='border-gray-600 h-[350px] w-[400px] ml-[130px] mt-[20px]'>
-                <div className='text-black w-[400px] h-[80px] text-5xl text-center mt-[50px] ml-[px] pl-[2px] font-bold border-gray-600 font-nunito'>Get the app!</div>
+                <div className='text-black w-[400px] h-[80px] text-5xl text-center mt-[50px] ml-[px] pl-[2px] 
+                font-bold border-gray-600 font-nunito'>Get the app!</div>
                 <div className='mt-[10px] ml-[80px] cursor-pointer w-[102px] h-[52px]'>
                     <Link href='https://play.google.com/store/games'>
                         <Image src="/../public/playstore.png" alt="playstore" width='100px' height='50px' />
