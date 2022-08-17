8142c86bc118fac407f0fb447bd360e9c6cb0ff9
[Kevin Osei Kwaku] <[knokwaku@gmail.com]>
2022-08-17 13:48:17 -0700

    changed about

--- components/About.jsx
+++ components/About.jsx
@@ -8,7 +8,8 @@ const About = () => {
                 <div className='font-nunito font-bold pl-[90px] text-[#0C7354] h-[75px] text-5xl border-gray-600'>
                     <h2>About Orion Meet</h2>
                 </div>
-                <div className='h-[200px] ml-[90px] w-[400px] font-semibold text-xl mt-[40px] text-[#0C7354] font-nunito text-center border-gray-600'>
+                <div className='h-[200px] ml-[90px] w-[400px] font-semibold text-xl mt-[40px] 
+                text-[#0C7354] font-nunito text-center border-gray-600'>
                     <p>Orion Meet is an app based platform for matching people based on their common likes and interests. Unlike most other match making apps, we don’t only focus on pairing people up , we allow the option to join groups full of like minded people for good networking.</p></div>
             </div>
             <div className='w-[690px] h-[550px] mt-[40px] ml-[60px] border border-red-600'>
