1144970f38356d598dfb71933da0885c7a64ea8d
[Kevin Osei Kwaku] <[knokwaku@gmail.com]>
2022-08-17 14:46:30 -0700

    fixed apostrophe error

--- components/Question.jsx
+++ components/Question.jsx
@@ -7,7 +7,7 @@ const Question = () => {
       <div className='w-[600px] h-[375px] mt-[150px] ml-[100px] border-red-600'>
         <p className='text-6xl text-[#1C5677] text-center font-semibold font-nunito'>
           SO WHAT ARE YOU WAITING FOR? COME ON BOARD AND SEE THE BEST FRIENDS
-          YOU'RE YET TO MEET.
+          YOU&apos;RE YET TO MEET.
         </p>
       </div>
       <div className='w-[600px] h-[550px] mt-[50px] ml-[100px] border border-red-600'>
