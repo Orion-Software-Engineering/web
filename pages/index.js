48f0b8071c806d72d0ae57581cf84a60755e7c5a
Hansen Koduah <63653407+hanskod@users.noreply.github.com>
2022-08-23 12:00:23 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
 import React from "react";
-import Hero from "../components/Hero";
-import Header from "../components/Header";
-import About from "../components/About";
-import Chat from "../components/Chat";
-import Download from "../components/Landing Page/Download";
-import Find from "../components/Find";
-import Getapp from "../components/Getapp";
-import Profiles from "../components/Profiles";
-import Question from "../components/Question";
+import Hero from "../components/Landing Page/Hero";
+import Header from "../components/Landing Page/Header";
+import About from "../components/Landing Page/About";
+import Chat from "../components/Landing Page/Chat";
+import Find from "../components/Landing Page/Find";
+import Getapp from "../components/Landing Page/Getapp";
+import Profiles from "../components/Landing Page/Profiles";
+import Question from "../components/Landing Page/Question";
 import Head from "next/head";
 
 export default function Home() {
