386d2f054065b6650a76293104e7854824594a7f
Hansen Koduah <63653407+hanskod@users.noreply.github.com>
2022-08-23 11:57:31 +0000

    updated files

--- pages/events.js
+++ pages/events.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import TopEvents from '../components/TopEvents'
+import EventHeader from '../components/EventHeader'
+import EventFooter from '../components/EventFooter'
 
-const events = () => {
-  return (
-    <div>events</div>
-  )
+export default function Event() {
+    return (
+        <div className=" h-full ">
+            <div className="h-full bg-opacity-90 bg-[#F6F6F6]">
+                <EventHeader />
+                <TopEvents />
+                <EventFooter />
+            </div>
+        </div>
+    )
 }
-
-export default events
