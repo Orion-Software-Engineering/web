c932a4587621e273d0915c2ef234187882f904e1
Kevin Osei Kwaku <knokwaku@gmail.com>
2022-09-17 14:31:15 -0700

    addded functionality of next button

--- components/events/form/EventForm2.jsx
+++ components/events/form/EventForm2.jsx
@@ -1,8 +1,70 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import Image from 'next/image'
+import { BiCalendar, BiTime } from 'react-icons/bi'
+import DatePicker from "../DatePicker";
 
-const EventForm2 = () => {
+const EventForm2 = ({ step, updateStep }) => {
+
+  const formRef = useRef()
+  const [date, setDate] = useState(new Date())
+  const [value, onChange] = useState('10:00');
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    // TODO: perform the push to backend here
+    console.log("submitted", formRef.current);
+  }
+
   return (
-    <div>EventForm2</div>
+    <div className="justify-self-center self-center flex  border-gray-600 h-[400px] ">
+
+      <div className="flex flex-col h-[400px] justify-center items-center  border-blue-600 w-[400px] rounded-l-3xl ">
+        <form ref={formRef} handleSubmit={handleSubmit} className="flex flex-col mb-[30px]" >
+          <label for="first" className='mt-[20px]'>Organizers</label>
+          <input type="text" id="first" name="first" className="bg-black h-[30px] w-[300px] text-white pl-[10px] rounded-lg" />
+          <label for="second" className="mt-[20px]">Date</label>
+          <div className='w-[300px] bg-black h-[30px] rounded-lg text-gray-300 flex flex-row-reverse items-center'>
+            <div className='p-2'
+              onClick={() => setDate(!date)}>
+              <div className='flex'><DatePicker /><BiCalendar size={22} /></div>
+            </div>
+          </div>
+          <div className="mt-[20px] border-blue-400">
+            <label for="third" className="">Time</label>
+            <div className='w-[100px] bg-black h-[30px] rounded-lg text-gray-300 flex flex-row-reverse items-center'>
+              <div className='p-2'
+                onClick={() => onChange(!value)}>
+                <div className=""><BiTime size={22} /></div>
+              </div>
+            </div>
+          </div>
+        </form>
+
+        <div className='flex flex-row w-[300px] mt-[50px] border-green-700 justify-between '>
+          <p className="self-start cursor-pointer"
+            onClick={() => updateStep(step - 1)}>
+            {'<'}Back
+          </p>
+          <p className="self-end cursor-pointer"
+            onClick={() => { updateStep(step + 1) }}>
+            Next{'>'}
+          </p>
+        </div>
+        <p className='mt-[0px] text-center'>{step}/4</p>
+  
+      </div>
+
+      {/* <div className=" border-blue-600 w-[400px] bg-[#1950DE] flex flex-col gap-y-2 justify-center items-center rounded-r-3xl">
+        <div>
+          <p className=" border-blue-600 text-center text-xl ">Upload Event Flyer</p>
+        </div>
+        <div className='bg-[#CDC5C5] border-blue-700 w-[250px]  h-[300px] flex justify-center items-center'>
+          <div className='w-[75px] '>
+            <Image src="/../public/camera.png" alt="camera" width='75px' height='75px' />
+          </div>
+        </div>
+      </div> */}
+    </div>
   )
 }
 
--- components/events/form/EventForm3.jsx
+++ components/events/form/EventForm3.jsx
@@ -1,8 +1,64 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import Image from 'next/image'
+import { BiCalendar, BiTime } from 'react-icons/bi'
 
-const EventForm3 = () => {
+
+
+
+
+const EventForm3 = ({ step, updateStep }) => {
+
+  const formRef = useRef()
+  const [checked, setChecked] = useState(true);
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    // TODO: perform the push to backend here
+    console.log("submitted", formRef.current);
+  }
+
+
   return (
-    <div>EventForm3</div>
+    <div className="text-black justify-between border border-gray-700 flex font-nunito ">
+
+      <div className="justify-self-center self-center flex  border-gray-600 h-[400px] ]">
+        <div className="flex flex-col h-[400px] justify-center items-center  border-blue-600 w-[400px] rounded-l-3xl ">
+          <form ref={formRef} handleSubmit={handleSubmit} className="flex flex-col mb-[30px]" >
+            <label for="first">Age Restrictions</label>
+            <div>18 plus ?</div>
+            <label for="second" className="mt-[20px]">MCs</label>
+            <input type="text" id="first" name="first" className="bg-black h-[30px] w-[300px] text-white pl-[10px] rounded-lg" />
+            <label for="third" className="mt-[20px]">Guests</label>
+            <input type="text" id="first" name="first" className="bg-black h-[30px] w-[300px] text-white pl-[10px] rounded-lg" />
+            <label for="third" className="mt-[0px] text-xs text-[#575757]">Add another guest</label>
+          </form>
+          <p className=" border border-blue-500  "></p>
+          <div className="flex flex-row w-[300px] justify-between ">
+            <p className="self-start cursor-pointer"
+              onClick={() => updateStep(step - 1)}>
+              {'<'}Back
+            </p>
+            <p className="self-end cursor-pointer"
+              onClick={() => { updateStep(step + 1) }}>
+              Next{'>'}
+            </p>
+          </div>
+          <p>{step}/4</p>
+        </div>
+
+        {/* <div className=" border-blue-600 w-[400px] bg-[#1EA313] flex flex-col gap-y-2 justify-center items-center rounded-r-3xl">
+          <div>
+            <p className=" border-blue-600 text-center text-xl ">Upload Event Flyer</p>
+          </div>
+          <div className='bg-[#CDC5C5] border-blue-700 w-[250px]  h-[300px] flex justify-center items-center'>
+            <div className='w-[75px]'>
+              <Image src="/../public/camera.png" alt="camera" width='75px' height='75px' />
+            </div>
+          </div>
+        </div> */}
+      </div>
+
+
+    </div >
   )
 }
 
