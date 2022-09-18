1e5184bc4196bac4fd8525ca521ffb4971088433
Kelvin Nii Lantei Lamptey <108931083+CeoBlunt@users.noreply.github.com>
2022-09-18 07:33:07 +0000

    added sample login and signup

--- components/accountBox/Index.jsx
+++ components/accountBox/Index.jsx
@@ -1,6 +1,8 @@
-import { cleanTrailingZeroInNumericSectionValue } from "@mui/x-date-pickers/internals/hooks/useField/useField.utils";
-import react from "react ";
+// import { cleanTrailingZeroInNumericSectionValue } from "@mui/x-date-pickers/internals/hooks/useField/useField.utils";
 import styled from "styled-components";
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+import { AccountContext } from "./accountContext";
 
 const BoxContainer = styled.div`
   width: 280px;
@@ -13,32 +15,38 @@ const BoxContainer = styled.div`
   position: relative;
   overflow: hidden;
 `;
-const TopContainer=styled.div`
-width:100%;
-height:250px;
-flex-direction: column;
-justify-content: flex-end;
-padding-bottom: 5em;
+const TopContainer = styled.div`
+  width: 100%;
+  height: 250px;
+  flex-direction: column;
+  justify-content: flex-end;
+  padding: 0 1.8em;
+  padding-bottom: 5em;
 `;
 
-const BackDrop = styled.div`
-width:160%;
-height:550px;
-position: absolute;
-display: flex;
-flex-direction: column;
-border-radius: 50%;
-transform: rotate(60deg);
-top: -290px;
-left: -70px;
-background: rgb(131,58,180);
-background: linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 46%, rgba(252,178,69,1) 100%);
+const BackDrop = styled(motion.div)`
+  width: 160%;
+  height: 550px;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  border-radius: 50%;
+  transform: rotate(60deg);
+  top: -290px;
+  left: -70px;
+  background: rgb(131, 58, 180);
+  background: linear-gradient(
+    90deg,
+    rgba(131, 58, 180, 1) 0%,
+    rgba(253, 29, 29, 1) 46%,
+    rgba(252, 178, 69, 1) 100%
+  );
 `;
 
-const HeaderContainer= styled.div`
-width: 100%;
-display: flex;
-flex-direction: column;
+const HeaderContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
 `;
 
 const HeaderText = styled.h2`
@@ -51,25 +59,98 @@ margin: 0;
 `;
 
 const SmallText = styled.h5`
-color: #fff;
-font-weight: 500;
-font-size: 11px;
-z-index: 10;
-margin: 0;
-margin-top: 10px;
-
-
+  color: #fff;
+  font-weight: 500;
+  font-size: 11px;
+  z-index: 10;
+  margin: 0;
+  margin-top: 10px;
 `;
- 
-export function AccountBox(props) {
-    return<BoxContainer>
+
+const InnerContainer = styled.div`
+  width: 100%;
+  display: fle;
+  flex-direction: column;
+  padding: 0 1.8em;
+`;
+
+const backdropVariants = {
+  expanded: {
+    width: "233%",
+    height: "1050px",
+    borderRadius: "20%",
+    transform: "rotate(60deg)",
+  },
+  collapsed: {
+    width: "160%",
+    height: "550px",
+    borderRadius: "50%",
+    transform: "rotate(60deg)",
+  },
+};
+
+const expandingTransition = {
+  type: "spring",
+  duration: 2.3,
+  stiffness: 30,
+};
+
+export default function AccountBox(props) {
+  const [isExpanded, setExpanded] = useState(false);
+  const [active, setActive] = useState("Signin");
+
+  const playExpandingAnimation = () => {
+    setExpanded(true);
+    setTimeout(() => {
+      setExpanded(false);
+    }, expandingTransition.duration * 1000 - 1500);
+  };
+
+  const switchToSignup = () => {
+    playExpandingAnimation();
+    setTimeout(() => {
+      setActive("signup");
+    }, 400);
+  };
+
+  const switchToSignin = () => {
+    playExpandingAnimation();
+    setTimeout(() => {
+      setActive("signin ");
+    }, 400);
+  };
+  const contextValue = { switchToSignup, switchToSignin };
+
+  return (
+    <AccountContext.Provider value={contextValue}>
+      <BoxContainer>
         <TopContainer>
-            <Backdrop/> 
+          <BackDrop
+            initial={false}
+            animate={isExpanded ? "expanded" : "collapsed"}
+            variants={backdropVariants}
+            transition={expandingTransition}
+          />
+          {active === "signin" && (
             <HeaderContainer>
-                <HeaderText>Welcome</HeaderText>
-                <HeaderText>Back</HeaderText>
-                <SmallText> Please sign-in to continue!</SmallText>
-                </HeaderContainer>   
+              <HeaderText>Welcome</HeaderText>
+              <HeaderText>Back</HeaderText>
+              <SmallText> Please sign-in to continue!</SmallText>
+            </HeaderContainer>
+          )}
+          {active === "signup" && (
+            <HeaderContainer>
+              <HeaderText>Create</HeaderText>
+              <HeaderText>Account</HeaderText>
+              <SmallText> Please sign-up to continue!</SmallText>
+            </HeaderContainer>
+          )}
         </TopContainer>
-     </BoxContainer>
+        <InnerContainer>
+          {active === "signin" && <LoginForm />}
+          {active === "signup" && <SignupForm />}
+        </InnerContainer>
+      </BoxContainer>
+    </AccountContext.Provider>
+  );
 }
