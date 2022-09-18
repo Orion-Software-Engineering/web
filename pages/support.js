eb09b2e70fd758bd23b2d94bf2beaa45ee0900ef
Hansen Koduah <63653407+hanskod@users.noreply.github.com>
2022-09-18 14:00:15 +0000

    corrected errors

--- pages/support.js
+++ pages/support.js
@@ -1,16 +1,13 @@
 import React from "react";
 import Header from "../components/home/Header";
-import Form from "../components/support/Form"
-import Accordion from "../components/support/Accordion"
-
-
+import Form from "../components/support/Form";
+import Accordion from "../components/support/Accordion";
 
 export default function Support() {
   return (
     <div>
-
       <Header />
-      <Accordion />
+      {/* <Accordion /> */}
       <Form />
     </div>
   );
