398be4697e18db6f280e0882ff238704d14848e1
Kevin Osei Kwaku <knokwaku@gmail.com>
2022-09-19 15:56:53 -0700

    modified date and time pickers on eventform 2

--- components/events/form/Completion.jsx
+++ components/events/form/Completion.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
-const Completion = () => {
+const Completion = ({ step, updateStep }) => {
   return (
-    <div>Completion</div>
+    <div className='flex flex-col justify-center items-center h-[400px] w-[400px]'>
+
+      <div className=''>
+        <p className="text-3xl font-extrabold font-Nunito">Your Event Is Live</p>
+      </div>
+
+      <div className='mt-16 cursor-pointer bg-black text-gray-200 text-xs w-36
+      rounded-md h-8 flex justify-center items-center shadow-xl hover:bg-orange-300 transition duration-300 hover:text-gray-800 select-none'>
+        <p className='px-2'>
+          {`View Events`}
+        </p>
+      </div>
+
+      <div className='flex flex-row w-[300px] mt-[50px] border-green-700 justify-between '>
+        <p className="self-start cursor-pointer"
+          onClick={() => updateStep(step - 1)}>
+          {'<'}Back
+        </p>
+      </div>.
+
+    </div>
+
   )
 }
 
--- components/events/form/EventForm2.jsx
+++ components/events/form/EventForm2.jsx
@@ -6,6 +6,7 @@ import DatePicker from "../DatePicker";
 const EventForm2 = ({ step, updateStep }) => {
 
   const formRef = useRef()
+
   const [date, setDate] = useState(new Date())
   const [value, onChange] = useState('10:00');
 
@@ -15,6 +16,16 @@ const EventForm2 = ({ step, updateStep }) => {
     console.log("submitted", formRef.current);
   }
 
+  const CustomTimePicker = () => {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+
+
+
   return (
     <div className="justify-self-center self-center flex  border-gray-600 h-[400px] ">
 
@@ -23,20 +34,71 @@ const EventForm2 = ({ step, updateStep }) => {
           <label for="first" className='mt-[20px]'>Organizers</label>
           <input type="text" id="first" name="first" className="bg-black h-[30px] w-[300px] text-white pl-[10px] rounded-lg" />
           <label for="second" className="mt-[20px]">Date</label>
-          <div className='w-[300px] bg-black h-[30px] rounded-lg text-gray-300 flex flex-row-reverse items-center'>
+          <div className='flex justify-between bg-black text-white rounded-md px-3 '>
+            <div className="flex flex-col gap-y-1">Day
+              <select id="day" name="hourtime" form="Dateform"
+                className='h-auto w-12 bg-black ' >
+                {Array.from({ length: 31 }, (x, i) => (
+                  <option>{i + 1}</option>))
+                }
+              </select>
+            </div>
+            <div className="flex flex-col gap-y-1">Month
+              <select id="month" name="hourtime" form="Dateform"
+                className='h-auto w-12 bg-black pb-2 ' >
+                {Array.from({ length: 12
+                 }, (x, i = 1) => (
+                  <option>{i + 1}</option>))
+                }
+              </select>
+            </div>
+            <div className="flex flex-col gap-y-1 text-center"> Year
+              <select id="year" name="hourtime" form="Dateform" className='h-auto w-14 bg-black' >
+                <option>2022</option>
+                <option>2023</option>
+                <option>2024</option>
+                <option>2025</option>
+              </select></div>
+          </div>
+          {/* <div className='w-[300px] bg-black h-[30px] rounded-lg text-gray-300 flex flex-row-reverse items-center'>
             <div className='p-2'
               onClick={() => setDate(!date)}>
-              <div className='flex'><DatePicker /><BiCalendar size={22} /></div>
+              <div className='flex'><BiCalendar size={22} /></div>{<DatePicker /> 
             </div>
-          </div>
+          </div> */}
+
           <div className="mt-[20px] border-blue-400">
             <label for="third" className="">Time</label>
-            <div className='w-[100px] bg-black h-[30px] rounded-lg text-gray-300 flex flex-row-reverse items-center'>
-              <div className='p-2'
-                onClick={() => onChange(!value)}>
+            <div className='flex justify-between bg-black text-white rounded-md px-3 '>
+              <div className="flex flex-col gap-y-1">Hour
+                <select id="hour" name="hourtime" form="timeform"
+                  // size={5} 
+                  // onfocus='this.size=10;' onblur='this.size=1;' onchange='this.size=1; this.blur();'
+                  className='h-auto w-12 bg-black ' >
+                  {Array.from({ length: 12 }, (x, i) => (
+                    <option>{i + 1 > 9 ? "" : "0"}{i + 1}</option>))
+                  }
+                </select>
+              </div>
+              <div className="flex flex-col gap-y-1">Minute
+                <select id="hour" name="hourtime" form="timeform"
+                  className='h-auto w-12 bg-black pb-2 ' >
+                  {Array.from({ length: 60 }, (x, i) => (
+                    <option>{i + 1 > 9 ? "" : "0"}{i + 1}</option>))
+                  }
+                </select>
+              </div>
+              <div className="flex flex-col gap-y-1 pr-1 text-left">AM
+                <select id="hour" name="hourtime" form="timeform" className='h-auto w-12 bg-black' >
+                  <option>AM</option>
+                  <option>PM</option>
+                </select></div>
+            </div>
+            {/* <div className='w-[100px] bg-black h-[30px] rounded-lg text-gray-300 flex flex-row-reverse items-center relative'>
+              <div className='p-2'>
                 <div className=""><BiTime size={22} /></div>
               </div>
-            </div>
+            </div> */}
           </div>
         </form>
 
@@ -51,7 +113,7 @@ const EventForm2 = ({ step, updateStep }) => {
           </p>
         </div>
         <p className='mt-[0px] text-center'>{step}/4</p>
-  
+
       </div>
 
     </div>
