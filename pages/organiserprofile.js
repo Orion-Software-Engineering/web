7351fa62cdbff65ad846db6e528b60485517fcf3
Hansen Koduah <63653407+hanskod@users.noreply.github.com>
2022-09-19 20:38:42 +0000

    updates

--- pages/organiserprofile.js
+++ pages/organiserprofile.js
@@ -4,14 +4,11 @@ import Header from "../components/home/Header";
 
 
 
-export  default function OrganiserProfile() {
-    return (
- <div>
-    <organiserProfile />
-    <Header />
-
- </div>
-
-
-    );
+export default function OrganiserProfile() {
+   return (
+      <div>
+         <Header />
+         <organiserProfile />
+      </div>
+   );
 }
