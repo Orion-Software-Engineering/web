d09d198678ee8aba19d7db5da6bd009c0cc9de1d
Kelvin Nii Lantei Lamptey <108931083+CeoBlunt@users.noreply.github.com>
2022-09-21 00:41:17 +0000

    Updated About Page

--- pages/about.js
+++ pages/about.js
@@ -2,11 +2,16 @@ import React from "react";
 import About from "../components/home/About";
 import Header from "../components/home/Header";
 
+import Profiles from "../components/home/Profiles";
 export default function AboutPage() {
   return (
     <div>
       <Header />
       <About />
+      
+      
+      
+      
     </div>
   );
 }
