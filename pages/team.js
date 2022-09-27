da4df039969c92b8bdf3b4b006f6e148e1860bac
Kelvin Nii Lantei Lamptey <108931083+CeoBlunt@users.noreply.github.com>
2022-09-27 00:10:09 +0000

    Update team.js

--- pages/team.js
+++ pages/team.js
@@ -1,14 +1,17 @@
 import React from "react";
 import Header from "../components/home/Header";
 import Team from "../components/home/Team"
+import NoSSR from "react-no-ssr";
 
 
 
 
 export default function Home() {
     return (
-        <div> 
-            <Header />
+        <div>
+            <NoSSR>
+                <Header />
+            </NoSSR>
             <Team />
 
         </div>
