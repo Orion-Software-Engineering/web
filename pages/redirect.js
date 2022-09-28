6506116007cb48b962145edd40109eac36b799a4
Kelvin Nii Lantei Lamptey <108931083+CeoBlunt@users.noreply.github.com>
2022-09-28 13:41:33 +0000

    UserRedirect Page

---
+++ pages/redirect.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/home/Header";
+import User from "../components/UserRedirect"
+
+
+
+export default function Continue() {
+    return (
+  <div>
+      <Header />
+                <User />
+  </div>
+
+
+    );
+}
