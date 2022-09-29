4aa8c993034de17a85aacc410592e067eee5cad6
Hansen Koduah <63653407+hanskod@users.noreply.github.com>
2022-09-29 08:01:18 +0000

    Update Org.jsx

--- components/Org.jsx
+++ components/Org.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const UserRedirect = () => {
   return (
     <div>
-      <div className="w-full h-screen text-center bg-[#0a192f] mt-[-64px]">
+      <div className="w-full h-screen text-center bg-[#0a192f] ">
         <div className="sm:flex-row flex-col inline-flex justify-center h-screen items-center mx-10 ">
           <div></div>
           <div className=" text-slate-400 max-w-[1000px] text-left mx-auto px-8 ">
