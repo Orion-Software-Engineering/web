be88d4b4066e1e36895f74348f454f065645136d
Kelvin Nii Lantei Lamptey <108931083+CeoBlunt@users.noreply.github.com>
2022-09-29 10:01:59 +0000

    bug fixes

--- components/home/About.jsx
+++ components/home/About.jsx
@@ -9,7 +9,7 @@ import {
 const About = () => {
   return (
     <div className="bg-[url('../public/bg2.jpg')] w-full h-screen bg-no-repeat bg-cover font-Nunito">
-      <div className="bg-[url('../public/bg2.jpg')] w-full bg-no-repeat bg-cover font-Nunito mt-[-64px]">
+      <div className="bg-[url('../public/bg2.jpg')] w-full bg-no-repeat bg-cover font-Nunito">
         <div className="w-full h-[700px] bg-gray-900/90 absolute"></div>
         <div className="max-w-[1240px] mx-auto text-white relative">
           <div className=" pt-20 px-4 py-12 ">
@@ -32,15 +32,11 @@ const About = () => {
                 <ChatIcon className="w-16 p-4 bg-red-400 text-white rounded-lg  mt-[-4rem] " />
                 <h3 className="font-bold text-2xl my-6">Matches & Events</h3>
                 <p className="text-gray-600 text-xl ">
-                
-                 Unlike other match making apps, it matches people based on their shared interests.
-                 It also gives community leaders and event organizers the platform to share and host events.
-                 Explore a variety of events of your interest.
-                 SignUp now and experience the best match making app.
-
-                  
-                  
-    
+                  Unlike other match making apps, it matches people based on
+                  their shared interests. It also gives community leaders and
+                  event organizers the platform to share and host events.
+                  Explore a variety of events of your interest. SignUp now and
+                  experience the best match making app.
                 </p>
               </div>
               <div className="bg-slate-100 pl-8 py-4  hover:bg-slate-400">
@@ -52,23 +48,22 @@ const About = () => {
               </div>
             </div>
             <div className="bg-white rounded-xl shadow-2xl">
-              <div className="p-8"> 
+              <div className="p-8">
                 <UserGroupIcon className="w-16 p-4 bg-red-400 text-white rounded-lg  mt-[-4rem] " />
                 <h3 className="font-bold text-2xl my-6">Our Team</h3>
                 <p className="text-gray-600 text-xl ">
-                  We are a sincere company with a straightforward vision.
-                  We believe that there is an amazing amount to be learned by working in a real world web production environment alongside other like  minded individuals.
-                  We keep culture and creativity at the heart of everything we do.
-                  
-                  
-              
+                  We are a sincere company with a straightforward vision. We
+                  believe that there is an amazing amount to be learned by
+                  working in a real world web production environment alongside
+                  other like minded individuals. We keep culture and creativity
+                  at the heart of everything we do.
                 </p>
               </div>
               <div className="bg-slate-100 pl-8 py-4  hover:bg-slate-400 ">
-              <a href={"/team"} >
-                <p className="flex items center text-red-300  ">
-                  Know us <ArrowSmRightIcon className="w-5 ml-2" />
-                </p>
+                <a href={"/team"}>
+                  <p className="flex items center text-red-300  ">
+                    Know us <ArrowSmRightIcon className="w-5 ml-2" />
+                  </p>
                 </a>
               </div>
             </div>
@@ -77,9 +72,11 @@ const About = () => {
                 <PhoneIcon className="w-16 p-4 bg-red-400 text-white rounded-lg  mt-[-4rem] " />
                 <h3 className="font-bold text-2xl my-6">Support</h3>
                 <p className="text-gray-600 text-xl ">
-                 Our support team is spread across the globe to give you answers fast.
-                 We are closer than you think.We also offer a wide variety of onlinde support through our website such as E-mail support
-                 and the ability to submit case online. And if you ever require emergency services were available 24/7.
+                  Our support team is spread across the globe to give you
+                  answers fast. We are closer than you think.We also offer a
+                  wide variety of onlinde support through our website such as
+                  E-mail support and the ability to submit case online. And if
+                  you ever require emergency services were available 24/7.
                 </p>
               </div>
               <div className="bg-slate-100 pl-8 py-4  hover:bg-slate-400">
