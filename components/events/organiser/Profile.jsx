f7eaab5f323ecbbfa1a03e66f8bd1d1cb815cc15
Hansen Koduah <63653407+hanskod@users.noreply.github.com>
2022-09-29 04:05:24 +0000

    Update Profile.jsx

--- components/events/organiser/Profile.jsx
+++ components/events/organiser/Profile.jsx
@@ -52,6 +52,7 @@ export default function OrganiserProfile() {
           <Header />
           {/* Web */}
           <div className="md:contents hidden">
+            {window.location.replace("/")}
             <div className="px-[10%] w-full">
               <div className="h-screen items-center justify-center flex mt-[-64px] ">
                 <div className="flex space-x-[5%]  justify-center w-full items-center px-auto">
